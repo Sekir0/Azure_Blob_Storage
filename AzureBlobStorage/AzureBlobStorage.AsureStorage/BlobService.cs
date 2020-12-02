9dc0a33892a692f3bb49053b486902ccff94c146
Alexander <50077233+Sekir0@users.noreply.github.com>
2020-12-02 21:34:43 +0200

    Init

---
+++ AzureBlobStorage/AzureBlobStorage.AsureStorage/BlobService.cs
@@ -0,0 +1,53 @@
+﻿using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using AzureBlobStorage.Domain;
+using BlobInfo = AzureBlobStorage.Domain.BlobInfo;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureBlobStorage.AsureStorage
+{
+    public class BlobService : IBlobService
+    {
+        private readonly BlobServiceClient _blobServiceClient;
+
+        public BlobService(BlobServiceClient blobServiceClient)
+        {
+            _blobServiceClient = blobServiceClient;
+        }
+
+        public async Task<BlobInfo> GetBlobAsync(string name)
+        {
+            var containerClient = _blobServiceClient.GetBlobContainerClient("azureblob");
+            var blobClient = containerClient.GetBlobClient(name);
+            var blobDownloadInfo = await blobClient.DownloadAsync();
+            return new BlobInfo(blobDownloadInfo.Value.Content, blobDownloadInfo.Value.ContentType);
+        }
+
+        public async Task UploadContentBlobAsync(string content, string fileName)
+        {
+            var containerClient = _blobServiceClient.GetBlobContainerClient("azureblob");
+            var blobClient = containerClient.GetBlobClient(fileName);
+            var bytes = Encoding.UTF8.GetBytes(content);
+            using (var memoryStream = new MemoryStream(bytes))
+            {
+                await blobClient.UploadAsync(memoryStream, new BlobHttpHeaders { ContentType = fileName.GetContentType() });
+            }
+        }
+
+        public async Task UploadFileBlobAsync(string filePath, string fileName)
+        {
+            var containerClient = _blobServiceClient.GetBlobContainerClient("azureblob");
+            var blobClient = containerClient.GetBlobClient(fileName);
+            await blobClient.UploadAsync(filePath, new BlobHttpHeaders { ContentType = filePath.GetContentType() });
+        }
+
+        public async Task DeleteFileBlobAsync(string blobName)
+        {
+            var containerClient = _blobServiceClient.GetBlobContainerClient("azureblob");
+            var blobClient = containerClient.GetBlobClient(blobName);
+            await blobClient.DeleteIfExistsAsync();
+        }
+    }
+}
---
+++ AzureBlobStorage/AzureBlobStorage/Startup.cs
@@ -0,0 +1,49 @@
+using Azure.Storage.Blobs;
+using AzureBlobStorage.AsureStorage;
+using AzureBlobStorage.Domain;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+
+namespace AzureBlobStorage
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddSingleton(x => new BlobServiceClient(Configuration.GetValue<string>("AzureBlobStorageConnectionString")));
+            services.AddSingleton<IBlobService, BlobService>();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
